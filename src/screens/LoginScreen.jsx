dc9c6c03badca268909ae1f7fdc6b2792e514070
thiagofalcaosenac <thiago.falcao@alunos.sc.senac.br>
2024-04-02 20:05:56 -0300

    ajustes finais

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -2,7 +2,6 @@ import { View } from "react-native"; // NUNCA WEB SE ESTIVER WEB REMOVA
 import styles from "../config/styles";
 import { Button, Text, TextInput } from "react-native-paper";
 import { useState } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import auth from "../config/firebase";
 import { Image } from "expo-image";
@@ -12,51 +11,16 @@ export default function LoginScreen({ navigation }) {
   const [senha, setSenha] = useState("");
   const [loading, setLoading] = useState(false);
 
-  // useEffect é usado para executar código quando a tela é carregada
-  // useState(()=>{
-  //     // vou perguntar, o usuário está logado?
-  //     checkIfUserIsLogged();
-  // },[])
-
-  // // função para verificar se o usuário está logado
-  // function checkIfUserIsLogged() {
-  //     // vou pegar o usuário que está salvo no AsyncStorage
-  //     AsyncStorage.getItem('usuario')
-  //         .then((usuario) => {
-  //             if (usuario) {
-  //                 // se existir um usuário, vou direcionar para a tela TabNavigator
-  //                 navigation.navigate('TabNavigator');
-  //             }
-  //         });
-  // }
-
   const fazerLogin = async () => {
-    // console.log(email, senha);
+    console.log(email, senha);
     try {
       const usuario = await signInWithEmailAndPassword(auth, email, senha);
-      console.log(usuario);
       navigation.navigate("TabNavigator");
     } catch (error) {
+      alert("Login/Senha inválidos!");
       console.log(error);
     }
   };
-
-  // async function salvarNome() {
-  //     setLoading(true);
-  //     console.log("Salvando nome");
-  //     try {
-  //         const usuario = {
-  //             email,
-  //             senha
-  //         }
-  //         await AsyncStorage.setItem('usuario', JSON.stringify(usuario));
-  //         navigation.navigate('TabNavigator');
-  //         setLoading(false);
-
-  //     } catch (e) {
-  //         console.log(e);
-  //     }
-  // }
 
   return (
     <View style={styles.container}>
